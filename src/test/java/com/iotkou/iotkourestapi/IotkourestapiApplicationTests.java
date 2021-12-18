286ea556cb1f432d98c77c970c2d5bfcfcfc8d98
sabsay03 <sabrikusaygulmez@gmail.com>
2021-12-18 16:29:55 +0300

    iotkourestapi

---
+++ src/test/java/com/iotkou/iotkourestapi/IotkourestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iotkou.iotkourestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IotkourestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
